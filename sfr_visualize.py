9691c30f12f1ee3882fa094b82f71dfc862f83b1
Bryce Besler <babesler@ucalgary.ca>
2018-01-18 15:16:11 -0700

    STYLE: Moved tests outside of install directory

--- sfr_visualize.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys
-
-from StereoFlouroscopyRegistration.visualization.visualize import main
-
-if __name__ == "__main__":
-    sys.exit(main())
